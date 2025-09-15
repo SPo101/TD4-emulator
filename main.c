d889dcc27ee2c098aa566bb68757245e2929cd1e
SPo101 <density101utility@gmail.com>
2025-09-15 21:25:50 +0300

    .

--- main.c
+++ main.c
@@ -17,12 +17,33 @@
   ((byte) & 0x01 ? '1' : '0') 
 
 
+/*
+ADD A,Im - 0000_xxxx - 0x00 - V
+ADD B,Im - 0101_xxxx - 0x50 - V
+
+MOV A,Im - 0011_xxxx - 0x30 - V
+MOV B,Im - 0111_xxxx - 0x70 - V
+
+MOV A,B  - 0001_0000 - 0x10 - V
+MOV B,A  - 0100_0000 - 0x40 - V
+
+JMP Im   - 1111_xxxx - 0xf0 - V
+JNC Im   - 1110_xxxx - 0xe0 - V
+
+IN A     - 0010_0000 - 0x20 - V
+IN B     - 0110_0000 - 0x60 - V
+
+OUT B     - 1001_0000 - 0x90 - V
+OUT Im    - 1011_xxxx - 0xb0 - V
+*/
 
 typedef struct {
 	unsigned char A;
 	unsigned char B;
 	unsigned char PC;
 	unsigned char CF;
+	unsigned char input;
+	unsigned char output;
 } registers;
 
 
@@ -39,60 +60,99 @@ int main(){
 	TD4.B = 0x00;
 	TD4.PC = 0x00;
 	TD4.CF = 0x00;
+	TD4.input = 0x00;
+	TD4.output = 0x00;
+	
 
 	unsigned char *mem = calloc(MEM_SIZE, 1);
+	unsigned char instruction;
+	unsigned char data;
 
 
 	read_opcode(mem);
 
+	print_console(&TD4, mem);
 
-
+	return 0;
 	for(;;){
 			
 		print_console(&TD4, mem);
 
 
-/*
-ADD A,Im - 0000_xxxx - 0x00 - V
-ADD B,Im - 0101_xxxx - 0x50 - X
+		
+		instruction = *(mem+TD4.PC) & 0xf0;
+		data = *(mem+TD4.PC) & 0x0f;
+		
 
-MOV A,Im - 0011_xxxx - 0x30 - X
-MOV B,Im - 0111_xxxx - 0x70 - X
-
-MOV A,B  - 0001_0000 - 0x10 - X
-MOV B,A  - 0100_0000 - 0x40 - X
-
-JMP Im   - 1111_xxxx - 0xf0 - V
-JNC Im   - 1110_xxxx - 0xe0 - X
-
-IN A     - 0010_0000 - 0x20 - X
-IN B     - 0110_0000 - 0x60 - X
-
-OUT B     - 1001_0000 - 0x90 - X
-OUT Im    - 1011_xxxx - 0xb0 - X
-*/
-
-		switch( *(mem+TD4.PC) & 0xf0 ){
+		switch( instruction ){
 		case 0x00:
-			TD4.A += *(mem+TD4.PC) & 0x0f;
-			if( TD4.A == 0x10){
-				TD4.A = 0x00;
+			TD4.A += data;
+			if( TD4.A >= 0x10){
+				TD4.A &= 0x0f;
 				TD4.CF = 0x01;
 			}
-
 			break;
+
+		case 0x50:
+			TD4.B += data;
+			if( TD4.B >= 0x10){
+				TD4.B &= 0x0f;
+				TD4.CF = 0x01;
+			}
+			break;
+
+		case 0x30:
+			TD4.A = data; 
+			break;
+		
+		case 0x70:
+			TD4.B = data; 
+			break;
+
+		case 0x10:
+			TD4.A = TD4.B;	
+			break;
+
+		case 0x40:
+			TD4.B = TD4.A;	
+			break;
+
 		case 0xf0:
-			TD4.PC = *(mem+TD4.PC) & 0x0f;
+			TD4.PC = data;
 			TD4.PC --;
 			break;
+
+		case 0xe0:
+			if(!TD4.CF){
+				TD4.PC = data;
+				TD4.PC --;
+				TD4.CF = 0x00;
+			}
+			break;
+
+		case 0x20:
+			TD4.A = TD4.input;
+			break;
+
+		case 0x60:
+			TD4.B = TD4.input;
+			break;
+
+		case 0x90:
+			TD4.output = TD4.B;
+			break;
+
+		case 0xb0:
+			TD4.output = data;
+			break;
+
 		default:
 			break;
 		}
 
 
-		
 
-		sleep(2);
+		sleep(1);
 		TD4.PC++;	
 		TD4.PC %= MEM_SIZE;
 			
@@ -120,27 +180,46 @@ void print_console(registers *TD4, unsigned char *mem){
 
 	printf("\n");
 	for(int i=0; i<MEM_SIZE; i++){
-		if(!i)
+		switch(i){
+			case 0:
 			printf("\033[%dm%2d | "BYTE_TO_BINARY_PATTERN \
-				"\t\033[0m[A] = "BYTE_TO_BINARY_PATTERN \
-				"\t[B] = "BYTE_TO_BINARY_PATTERN \
+				"\t\033[0m[ A] = "BYTE_TO_BINARY_PATTERN \
+				"\t[ B] = "BYTE_TO_BINARY_PATTERN \
 				"\n", \
 				(i == TD4->PC) ? 31 : 0, i, BYTE_TO_BINARY(*(mem+i)), \
 				BYTE_TO_BINARY(TD4->A), BYTE_TO_BINARY(TD4->B));
+				break;
 
-		else if(i==1)
+			case 1:
 			printf("\033[%dm%2d | "BYTE_TO_BINARY_PATTERN \
 				"\t\033[0m[CF] = "BYTE_TO_BINARY_PATTERN \
 				"\t[PC] = "BYTE_TO_BINARY_PATTERN \
 				"\n", \
 				(i == TD4->PC) ? 31 : 0, i, BYTE_TO_BINARY(*(mem+i)), \
 				BYTE_TO_BINARY(TD4->CF), BYTE_TO_BINARY(TD4->PC));
-		else
-			printf("\033[%dm%2d | "BYTE_TO_BINARY_PATTERN"\n", \
-				(i == TD4->PC) ? 31 : 0, i, BYTE_TO_BINARY(*(mem+i)));
-		fflush(stdout);
-	}
-	printf("\033[18A");
-	printf("\n");
+				break;
 
+			case 3:
+			printf("\033[%dm%2d | "BYTE_TO_BINARY_PATTERN \
+				"\t\033[0m[out] = "BYTE_TO_BINARY_PATTERN \
+				"\n", \
+				(i == TD4->PC) ? 31 : 0, i, BYTE_TO_BINARY(*(mem+i)), \
+				BYTE_TO_BINARY(TD4->output));
+				break;
+
+			case MEM_SIZE-1:
+				printf("\033[%dm%2d | "BYTE_TO_BINARY_PATTERN \
+					"\tEnter input: ", \
+					(i == TD4->PC) ? 31 : 0, i, BYTE_TO_BINARY(*(mem+i)));
+				break;
+
+			default:
+				printf("\033[%dm%2d | "BYTE_TO_BINARY_PATTERN"\n", \
+
+					(i == TD4->PC) ? 31 : 0, i, BYTE_TO_BINARY(*(mem+i)));
+				break;
+		}
+	}
 }
+
+
