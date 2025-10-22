7cbd49eaf452e6c59160583d6883407c44d8fd94
SPo101 <density101utility@gmail.com>
2025-10-22 15:19:15 +0300

    Continue TD4m finished selectors, working on ALU, other logic of cpu.

--- src/advanced_emulator_m.cpp
+++ src/advanced_emulator_m.cpp
@@ -16,6 +16,16 @@ using namespace std;
 #define tobool(num) \
   ((num) & 0x01 ? 1 : 0)
 
+#define neg(num) \
+  ((num) & 0x80 ? '0' : '0'), \
+  ((num) & 0x40 ? '0' : '0'), \
+  ((num) & 0x20 ? '0' : '0'), \
+  ((num) & 0x10 ? '0' : '0'), \
+  ((num) & 0x08 ? '0' : '1'), \
+  ((num) & 0x04 ? '0' : '1'), \
+  ((num) & 0x02 ? '0' : '1'), \
+  ((num) & 0x01 ? '0' : '1') 
+
 typedef struct {
 	unsigned char A;
 	unsigned char B;
@@ -26,6 +36,7 @@ typedef struct {
 	unsigned char input;
 	unsigned char output;
 	unsigned char zero;
+	unsigned char one;
 
 	unsigned char *RAM;
 	unsigned char *work_register1;
@@ -33,39 +44,89 @@ typedef struct {
 	unsigned char *load_register;
 
 	unsigned char cnt_cf;
+	unsigned char cnt_zf;
 	unsigned char arithmetic_logic_act;//0-arithmetic 1-logic
-	unsigned char move_bits;//0 for mostly all reg, 1 for x, 2 for y,  3 for xy, pc etc
+	unsigned char move_bits;//0 for mostly all reg, 1 for x, 2 for y/pcl,  3 for xy, pc etc
 } registers;
 
 void opcode_decode(unsigned char *, registers *);
 unsigned char not_(unsigned char, int = 0);
 
-//remove later
-unsigned char command = '&';
+void arithmetic_unit(unsigned char *instruction, registers *TD4m){
+
+}
+
+void logic_unit(unsigned char *instruction, registers *TD4m){
+	switch(*instruction){
+		case 0x81: //A = -A ( A = neg(A) + 1)
+		case 0x82: //A = !A ( A = neg(A) + 0)
+			*TD4m->load_register = neg(*TD4m->work_register1) + *TD4m->work_register2;
+			break;
+		case 0x83: //A or B
+			*TD4m->load_register = *TD4m->work_register1 | *TD4m->work_register2;
+			break;
+		case 0x84: //A and B
+			*TD4m->load_register = *TD4m->work_register1 & *TD4m->work_register2;
+			break;
+		case 0x85: //A xor B
+			*TD4m->load_register = *TD4m->work_register1 ^ *TD4m->work_register2;
+			break;
+	}
+
+
+}
 
 int main(int argc, char *argv[]){
 
-	unsigned char inst = 0x8e;
+	unsigned char inst = 0x81;
 	registers TD4m;
-	TD4m.A = 'a';
-	TD4m.B = 'b';
-	TD4m.PC = 'p';
-	TD4m.XY = 'x';
-	TD4m.CF = 'c';
-	TD4m.ZF = 'z';
-	TD4m.input = 'i';
-	TD4m.output = 'o';
-	TD4m.zero =  '0';
+	TD4m.A = 'A';
+	TD4m.B = 'B';
+	TD4m.PC = 'P';
+	TD4m.XY = 0xff;
+	TD4m.CF = 'C';
+	TD4m.ZF = 'Z';
+	TD4m.input = 'I';
+	TD4m.output = 'O';
+	TD4m.zero =  0x00;
+	TD4m.one =  0x01;
+
+	TD4m.RAM = (unsigned char*) malloc(124);
 
 	opcode_decode(&inst, &TD4m);
 
-	cout << "summand = " << *TD4m.work_register1 << endl;
-	cout << " res in = " << *TD4m.load_register << endl;
+	printf("summand1 = %c (%hhx) \n", *TD4m.work_register1, *TD4m.work_register1);
+	if(TD4m.work_register2 != NULL)
+		printf("summand2 = %c (%hhx) \n", *TD4m.work_register2, *TD4m.work_register2);
+	if(TD4m.load_register != NULL)
+		printf("res = %c (%hhx) \n", *TD4m.load_register, *TD4m.load_register);
+	printf("mode = %c (%hhx) \n", TD4m.move_bits, TD4m.move_bits);
 
+
+	/*
+	need handler
+		*TD4m.work_register2, *TD4m.load_register
+	can be NULL
+	*/
 
 	//when we direct registers to arithmetic unit or logic unit
-	//if()
-	
+	if(((0x01 <= (inst&0x0f)) & ((inst&0x0f) <= 0x05)) & ((inst&0xf0) == 0x80))
+		logic_unit(&inst, &TD4m);
+	else
+		arithmetic_unit(&inst, &TD4m);	
+
+	/*
+	if(*TD4m->load_register >= 0x10){
+		*TD4m->load_register &= 0x0f;
+		TD4m->CF = 0x01;
+		TD4m->cnt_cf = 0x02;
+	}
+
+	if(*TD4m->load_register == 0x00){
+		TD4m->ZF = 0x01;
+		TD4m->cnt_zf = 0x02;
+	}
+	*/
 }
 
 void opcode_decode(unsigned char *instruction, registers *TD4m){ 
@@ -90,6 +151,8 @@ void opcode_decode(unsigned char *instruction, registers *TD4m){
 								(D7 & D6 & not_(D5));
 
 
+	TD4m->move_bits = 0;
+
 	if(is_command)
 		goto data_is_command;
 
@@ -97,10 +160,10 @@ void opcode_decode(unsigned char *instruction, registers *TD4m){
 	SUM1 |= (tobool(D5) << 5);
 	SUM1 |= (tobool(D7|D4) << 4);
 
-	LOAD |= (tobool(D7|D6) << 7);
-	LOAD |= (tobool(D7|not_(D6)) << 6);
-	LOAD |= (tobool(not_(D7 & not_(D6))) << 5);
-	LOAD |= (tobool(not_(D6 & D7 & (D4 | not_(TD4m->CF)))) << 4);
+	LOAD |= (tobool(D7|D6									) << 7);
+	LOAD |= (tobool(D7|not_(D6)								) << 6);
+	LOAD |= (tobool(not_(D7 & not_(D6))						) << 5);
+	LOAD |= (tobool(not_(D6 & D7 & (D4 | not_(TD4m->CF)))	) << 4);
 
 
 	//block when last 4 bits present data
@@ -118,7 +181,9 @@ void opcode_decode(unsigned char *instruction, registers *TD4m){
 			TD4m->work_register1 = &TD4m->zero;
 			break;
 	}
-	
+
+	TD4m->work_register2 = NULL;
+
 	switch(LOAD){//define where to store a result from ALU
 		case 0x70: //A register
 			TD4m->load_register = &TD4m->A;
@@ -139,21 +204,23 @@ void opcode_decode(unsigned char *instruction, registers *TD4m){
 	return;
 
 	//block when last 4 bits present command
+	//A-0000 B-0001 RAM-0010 XY-0011 X-0100 Y-0101 PC-0110 zero-0111 one-1000 out-1001
 data_is_command:
-	TD4m->work_register1 = &command;
-	TD4m->work_register2 = &command;
+	SUM1 |= (tobool(D3 & ((not_(D2) & not_(D0)) | (D2 & D1))) << 5);
+	SUM1 |= (tobool(D3 & D1 & ( (not_(D2) * D0) | D2 )		) << 4);
 
+	SUM2 |= (tobool((not_(D3) & not_(D2) & not_(D1) & D0) | (D3 & D2 & D1 & not_(D0))							) << 7);
+	SUM2 |= (tobool((not_(D3)&D1&((not_(D2)&not_(D0))|(D2&D0))) | (D3&not_(D2)) | (D3&D2&(not_(D1)|(D1&D0)))	) << 6);
+	SUM2 |= (tobool(SUM2>>6																						) << 5);
+	SUM2 |= (tobool(not_(SUM2>>7)																				) << 4);
 	
-	LOAD |= (tobool(not_(D3) & D2 & D1 & D0) << 7);
-	LOAD |= (tobool(D3 & D2 & not_(D1)) << 6);
-	LOAD |= (tobool(D3 & (not_(D2) & D0 | D2 & D1)) << 5);
+	LOAD |= (tobool(not_(D3) & D2 & D1 & D0										) << 7);
+	LOAD |= (tobool(D3 & D2 & not_(D1)											) << 6);
+	LOAD |= (tobool(D3 & ((not_(D2) & D0) | (D2 & D1))							) << 5);
 	LOAD |= (tobool( (D1 & ( (D3 & not_(D2) & not_(D0)) | \
 		(not_(D3) & D2 & D0))) | (D3 & D2 & ((not_(D1) & D0) | (D1 & not_(D0))))) << 4);
 
 	//define where to store a result from ALU	
-	//A-0000 B-0001 RAM-0010 XY-0011 X-0100 Y-0101 PC-0110 zero-0111 one-1000 out-1001
-
-	TD4m->move_bits = 0;
 	switch(LOAD){
 		case 0x00: //A register
 			TD4m->load_register = &TD4m->A;
@@ -181,20 +248,72 @@ data_is_command:
 			break;	
 		case 0x60: //PC
 			TD4m->load_register = &TD4m->PC;
-			TD4m->move_bits = 8;
+			TD4m->move_bits = 3;
 			break;	
 	}
 
-/*
+
 	//define 1st summand for ALU
-	switch(){
-		break;	
+	switch(SUM1){
+		case 0x00: //A register
+			TD4m->work_register1 = &TD4m->A;
+			break;	
+		case 0x10: //B register
+			TD4m->work_register1 = &TD4m->B;
+			break;	
+		case 0x20: //RAM
+			TD4m->work_register1 = (TD4m->RAM+TD4m->XY);
+			break;	
+		case 0x30: //XY
+			TD4m->work_register1 = &TD4m->XY;
+			break;	
 	}
+
+
 	//define 2nd summand for ALU
-	switch(){
-		break;	
+	switch(SUM2){
+		case 0x10: //B register
+			TD4m->work_register2 = &TD4m->B;
+			break;	
+		case 0x70: //ZERO
+			TD4m->work_register2 = &TD4m->zero;
+			break;	
+		case 0x80: //ONE
+			TD4m->work_register2 = &TD4m->one;
+			break;	
 	}
-*/
+
+	/*
+	printf(BYTE_TO_BINARY_PATTERN8"\n", BYTE_TO_BINARY8(SUM1));
+	printf(BYTE_TO_BINARY_PATTERN8"\n", BYTE_TO_BINARY8(SUM2));
+	printf(BYTE_TO_BINARY_PATTERN8"\n", BYTE_TO_BINARY8(LOAD));
+	*/
+
+	if( ((*instruction)&0xf0) == 0xa0){
+		TD4m->work_register1 = &TD4m->input;
+		TD4m->work_register2 = &TD4m->zero;
+		TD4m->load_register = NULL;
+
+		if(TD4m->ZF){
+			TD4m->load_register = &TD4m->PC;
+			TD4m->move_bits = 2;
+		}
+
+	}
+	if( ((*instruction)&0xf0) == 0xc0){
+		TD4m->work_register1 = &TD4m->input;
+		TD4m->work_register2 = &TD4m->zero;
+		TD4m->load_register = &TD4m->XY;
+		TD4m->move_bits = 2;
+		
+	}
+	if( ((*instruction)&0xf0) == 0xd0){
+		TD4m->work_register1 = &TD4m->input;
+		TD4m->work_register2 = &TD4m->zero;
+		TD4m->load_register = &TD4m->XY;
+		TD4m->move_bits = 1;
+		
+	}
 	return;
 }
 
