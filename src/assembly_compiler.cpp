4772b5cf62344d39843b513867748cfa4645fbef
SPo101 <density101utility@gmail.com>
2025-11-06 01:17:41 +0300

    Updata compiler. Now compiler can create opcode for both cpus td4 and td4m.

--- src/assembly_compiler.cpp
+++ src/assembly_compiler.cpp
@@ -3,188 +3,108 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <unordered_map>
+
 #define MEM_SIZE 16
 
 using namespace std;
 
-struct item{
-	string key;
-	unsigned value;
-};
-
-class hash_table{
-public:
-	vector <item*> table;
-	int size;
-	int count;
-
-	hash_table(int size_, int count_ = 0){
-		size = size_;
-		count = count_;
-		table.resize(size, nullptr);
-	}
-
-	int put_item(item item_){
-		int place = hash_function(item_.key);
-		int was_put = 0;
-		item *new_item = (item*) malloc(sizeof(item));
-		new_item->key = item_.key;
-		new_item->value = item_.value;
-
-		for(int i=1; i<=size; i++){
-			if( table[place] && table[place]->key.compare(new_item->key) != 0){
-				place++;
-				place%=size;
-			}	
-			else{
-				was_put = 1;
-				break;
-			}
-		}
-		if(!was_put)
-			return 0;
-
-		table[place] = new_item;
-		count++;
-		return 1;
-	}
-
-	int put_item(string const key_, unsigned char const value_){
-		int place = hash_function(key_);
-		int was_put = 0;
-		item *new_item = (item*) malloc(sizeof(item));
-		new_item->key = key_;
-		new_item->value = value_;
-
-		for(int i=1; i<=size; i++){
-			if( table[place] && table[place]->key.compare(new_item->key) != 0){
-				place++;
-				place%=size;
-			}	
-			else{
-				was_put = 1;
-				break;
-			}
-		}
-		if(!was_put)
-			return 0;
-
-		table[place] = new_item;
-		count++;
-		return 1;
-	}
-
-	int get_item(string const& key){
-		int place = hash_function(key);
-		int was_get = 0;
-
-		for(int i=1; i<=size; i++){
-			if(table[place]->key.compare(key) != 0){
-				place++;
-				place%=size;
-			}	
-			else{
-				was_get= 1;
-				break;
-			}
-		}
-		if(!was_get)
-			return -1;
-
-		return place;
-	}
-
-	void print_table(){
-		/*
-		for(item *item_ : table)
-			if(item_)
-				cout << item_->key << " " << item_->value << endl;
-		*/
-		for(int i=0; i<size; i++)
-			if(table[i])
-				printf("%2d %s \t %x\n", i, table[i]->key.c_str(), table[i]->value);
-		printf("\nsize=%d count=%d\n", size, count);
-	}
-
-	unsigned operator[](int position){
-		return table[position]->value;
-	}
-
-	unsigned operator[](string const &key){
-		return table[this->get_item(key)]->value;
-	}
-
-private:
-	//djb2 hash.
-    int hash_function(string const& key){
-        unsigned long hash = 5381;
-
-        for (int c : key)
-            hash = ((hash << 5) + hash) + c;
-
-        return hash%size;
-    }
-};
-
 int main(int argc, char *argv[]){
 
-	if(argc != 3)
-		printf("Usage: <progname> <file1> <file2>,\nwhere file1 is file with opcode, file2 is bin file.\n");
+	unordered_map<string, unsigned char> mnemo;
+	vector<string> inst;
 
-	hash_table mnemo(MEM_SIZE);
 
-	mnemo.put_item("ADD A,", 0x00);
-	mnemo.put_item("ADD B,", 0x50);
-	mnemo.put_item("MOV A,", 0x30);
-	mnemo.put_item("MOV B,", 0x70);
-	mnemo.put_item("MOV A,B",  0x10);
-	mnemo.put_item("MOV B,A",  0x40);
-	mnemo.put_item("JMP ",   0xf0);
-	mnemo.put_item("JNC ",   0xe0);
-	mnemo.put_item("In A",     0x20);
-	mnemo.put_item("In B",     0x60);
-	mnemo.put_item("OUT B",    0x90);
-	mnemo.put_item("OUT ",   0xb0);
+	mnemo["ADDA"] = 0x00;
+	mnemo["MOVAB"] = 0x10;
+	mnemo["INA"] = 0x20;
+	mnemo["MOVA"] = 0x30;
+	mnemo["MOVBA"] = 0x40;
+	mnemo["ADDB"] = 0x50;
+	mnemo["INB"] = 0x60;
+	mnemo["MOVB"] = 0x70;
 
-	string data_arr = "0123456789abcdef";
-	stringstream str_data;
-	unsigned data;
+	mnemo["ADDAB"] = 0x80;
+	mnemo["NEGA"] = 0x81;
+	mnemo["NOTA"] = 0x82;
+	mnemo["ORAB"] = 0x83;
+	mnemo["ANDAB"] = 0x84;
+	mnemo["XORAB"] = 0x85;
+	mnemo["SUBAB"] = 0x86;
+	mnemo["OUTA"] = 0x87;
+	mnemo["LDA"] = 0x88;
+	mnemo["STA"] = 0x89;
+	mnemo["LDB"] = 0x8a;
+	mnemo["STB"] = 0x8b;
+	mnemo["MOVXA"] = 0x8c;
+	mnemo["MOVYA"] = 0x8d;
+	mnemo["INCXY"] = 0x8e;
+	mnemo["JMPXY"] = 0x8f;
+
+	mnemo["OUTB"] = 0x90;
+	mnemo["JZ"] = 0xa0;
+	mnemo["OUT"] = 0xb0;
+	mnemo["MOVY"] = 0xc0;
+	mnemo["MOVX"] = 0xd0;
+	mnemo["JNC"] = 0xe0;
+	mnemo["JMP"] = 0xf0;
+
+
+	fstream infile (argv[1], ios::in);
+	fstream outfile (argv[2], ios::out | ios::binary | ios::trunc);
+
+	if(!infile.is_open())
+		exit(0);
+
+	int ln;
+	int data;
 	unsigned char out_value;
-	string num;
+	string token;
 
-	fstream inputdata (argv[1], ios::in);
-	fstream outputdata (argv[2], ios::out | ios::binary | ios::trunc);
+	for(string input; getline(infile, input);){
+		ln = input.length();
 
-	if(!inputdata.is_open())
-		; //handler
-
-	for(string line; getline(inputdata, line);){
-		if(!line.size())
+		if(!ln){
+			out_value = 0x00;
+			outfile.write(reinterpret_cast<const char*>(&out_value), sizeof(out_value));
 			continue;
-
-		num = "0";
-		if(data_arr.find(line.back()) != string::npos){
-			num = line.back();
-			line.pop_back();
 		}
 
-		if(mnemo.get_item(line) == -1)
-			; //handler
-		
-		str_data.clear();
-		str_data << hex << num;
-		str_data >> data;
+		for(int i=0; i<=ln; i++){
+			if((input[i] == ' ') | (input[i] == ',') | (input[i] == '\0')){
+				inst.push_back(token);
+				token.erase();
+			}
+			else
+				token.push_back(input[i]);
+		}
 
-		out_value = static_cast<unsigned char>(mnemo[line] + data);
-		outputdata.write(reinterpret_cast<const char*>(&out_value), sizeof(out_value));
+
+		ln = inst.size();
+		data = 0;
+		if(("A" > inst.at(ln-1)) | ("Z" < inst.at(ln-1))){
+			try{
+				data = stoul(inst.at(ln-1), nullptr, 16);
+			}
+			catch (invalid_argument& e){
+				data = 0;
+			}
+
+			for(int i=0; i<ln-1; i++)
+				token+=inst.at(i);
+		}
+		else
+			for(int i=0; i<ln; i++)
+				token+=inst.at(i);
+
+		printf("%s %x\n", token.c_str(), static_cast<unsigned char>(data) + mnemo[token]);
+
+		out_value = static_cast<unsigned char>(data) + mnemo[token];
+		outfile.write(reinterpret_cast<const char*>(&out_value), sizeof(out_value));
+
+		token.erase();
+		inst.clear();
 	}
-	inputdata.close();
-	outputdata.close();
+	infile.close();
+	outfile.close();
 }
-
-
-
-
-
-
-
